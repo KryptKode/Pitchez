7bc5a37d7252b03638485543637d3f996e61181a
kryptkode <paulakhamiogu@gmail.com>
2020-05-13 00:49:57 +0100

    chore: add code to record sound - rename the default view controller to RecordSoundViewController

--- PitchMod/RecordSoundViewController.swift
+++ PitchMod/RecordSoundViewController.swift
@@ -1,18 +1,21 @@
 //
-//  ViewController.swift
+//  RecordSoundViewController.swift
 //  PitchMod
 //
 //  Created by Paul on 09/05/2020.
 //  Copyright © 2020 Paul. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundViewController: UIViewController {
 
     @IBOutlet weak var recordStatusLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,19 @@ class ViewController: UIViewController {
         updateLabel(message: "Recording in progress")
         enableStopButton()
         disableRecordButton()
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSession.CategoryOptions.defaultToSpeaker)
+
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     @IBAction func stopRecord(_ sender: Any) {
         updateLabel(message: "Tap to record")
